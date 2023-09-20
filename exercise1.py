e6669485b5f512c180097454fd6f932ce217e37b
EdgarMC22 <143836543+EdgarMC22@users.noreply.github.com>
2023-09-19 21:13:28 -0500

    EECS 168 Project 6

--- exercise1.py
+++ exercise1.py
@@ -1,37 +1,86 @@
 '''
 Name: Edgar Mendez
 KUID: 3048729
-Date Created: 09/26/2022
-Last Modified: 09/26/2022
+Date: 10/22/21
+Lab: 06
+Last modified: 10/22/21
+Purpose: Digit Functions
 '''
-#exercise1.py
 
-def rec_power(i, base, temp, power):
-	if power == 0:
-		return 1
-	elif power < 0:
-		base = int(input("Enter a base: "))
-		power = int(input("Enter a power: "))
-		print("Sorry, your exponent must be zero or larger.")
-		rec_power(0, base, power)
-	elif power == 1:
-		return base
-	elif power == 2:
-		return base*base
-	elif i < power - 1:
-		base = base*temp
-		print(base)
-		rec_power(i + 1, base, temp, power)
+def last_digit(num):
+	num_list = list(str(num))
+	return num_list[-1]
 
+def remove_last_digit(num):
+	num_list = list(str(num))
+	if len(num_list) <= 1:
+		return 0
+	else:
+		num_list2 = num_list[:-1]
+		return num_list2
 
+def add_digit(current_num, new_digit):
+	num_list = list(str(current_num))
+	num_list[len(num_list):] = str(new_digit)
+	return num_list
 
+def reverse(num):
+	num_list = list(str(num))
+	new_list = []
+	temp = 1
+	while temp - 1 < len(num_list):
+		new_list[len(num_list):] = num_list[0 - temp]
+		temp = temp + 1
+	print(new_list)
+	return new_list
+
+def is_palindrome(num):
+	num_list = list(str(num))
+	list2 = reverse(num)
+	if num_list == list2:
+		print("It is a palindrome.")
+		return True
+	else:
+		print("It is not a palindrome.")
+		return False
+
+def count_digits(num):
+	num_list = list(str(num))
+	print(len(num_list))
+	return len(num_list)
+
+def sum_digits(num):
+	num_list = list(str(num))
+	sum = 0
+	for index in num_list:
+		sum = sum + int(index)
+	print(sum)
+	return sum
+
+def print_menu():
+	print("1) Count digits")
+	print("2) Sum digits")
+	print("3) Is palindrome")
+	print("4) Reverse")
+	print("5) Exit")
 
 def main():
-	base = int(input("Enter a base: "))
-	power = int(input("Enter a power: "))
-	if isinstance(base, int) == False or isinstance(power, int) == False or base == None or power == None:
-		raise RuntimeError
-	else:
-		rec_power(0, base, base, power)
-
+	choice = 0
+	print_menu()
+	while choice != 5:
+		choice = int(input("Choice: "))
+		if choice == 1:
+			digits = int(input("Enter your digits: "))
+			count_digits(digits)
+		if choice == 2:
+			digits = int(input("Enter your digits: "))
+			sum_digits(digits)
+		if choice == 3:
+			digits = int(input("Enter your digits: "))
+			is_palindrome(digits)
+		if choice == 4:
+			digits = int(input("Enter your digits: "))
+			reverse(digits)
+		print_menu()
+	print("Exiting...")
 main()
